2d5f7ba43771b77b98885d90f0436afd734f05fc
Cristiane11 <bjwmcd@yahoo.com>
2020-06-13 22:52:35 -0400

    update

---
+++ front-end-portfolio/src/components/ui/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+
+function Header() {
+    return (
+      <div className="">
+        <AppBar position="static">
+  <Toolbar>
+    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <MenuIcon />
+    </IconButton>
+    <Typography variant="h6" className={classes.title}>
+      News
+    </Typography>
+    <Button color="inherit">Login</Button>
+  </Toolbar>
+</AppBar>
+      </div>
+    );
+  }
+  
+  export default Header;
