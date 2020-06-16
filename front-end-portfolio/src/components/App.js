89b0ded702937aeb1de11d05ba48b033c310428c
Cristiane11 <bjwmcd@yahoo.com>
2020-06-16 06:10:42 -0400

    update

--- front-end-portfolio/src/components/App.js
+++ front-end-portfolio/src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import Header from './ui/Header';
+import {ThemeProvider } from '@material-ui/core/styles';import Header from './ui/Header';
+
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider>
+    <div className="">
       <Header/>
       <p>Cristiane Abreu</p>
+      <hr/>
     </div>
+    </ThemeProvider>
   );
 }
 
