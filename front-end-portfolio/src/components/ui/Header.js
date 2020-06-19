0860d55df18360a8231d7ea3c348d2f1198518c0
Cristiane11 <bjwmcd@yahoo.com>
2020-06-19 07:53:45 -0400

    update

--- front-end-portfolio/src/components/ui/Header.js
+++ front-end-portfolio/src/components/ui/Header.js
@@ -3,6 +3,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
 function ElevationScroll(props) {
     const { children} = props;
@@ -22,7 +23,7 @@ function Header(props) {
         <ElevationScroll>
         <AppBar position="fixed" color="primary">
             <Toolbar>
-              <Typography variant ="h6">
+              <Typography variant ="h6" >
               <h1>EU</h1>  
               </Typography>
                 
